6b5b904926495dd8f42b02232aaaad26a8ba2f21
Abiola Adeosun <abiolaolalekan39@gmail.com>
2025-05-29 12:16:01 +0100

    changed to pnpm package manager

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
         {/* Background Image */}
         <div className="absolute inset-0 z-0">
           <Image
-            src="/images/can-pickup.png"
+            src="/images/hero-cleanup.png"
             alt="Community cleanup effort"
             fill
             className="object-cover object-center"
